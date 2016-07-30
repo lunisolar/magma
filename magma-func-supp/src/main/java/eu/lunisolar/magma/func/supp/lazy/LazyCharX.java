131f8c08d373d92d5a6d76ad2b4c2ebde0421495
lunisolar <open@lunisolar.eu>
2016-07-30 18:58:50 +0200

    moving meta model to Java

---
+++ magma-func-supp/src/main/java/eu/lunisolar/magma/func/supp/lazy/LazyCharX.java
@@ -0,0 +1,100 @@
+/*
+ * This file is part of "lunisolar-magma".
+ *
+ * (C) Copyright 2014-2016 Lunisolar (http://lunisolar.eu/).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package eu.lunisolar.magma.func.supp.lazy;
+
+import javax.annotation.Nonnull; // NOSONAR
+import javax.annotation.Nullable; // NOSONAR
+import java.util.Objects; // NOSONAR
+import eu.lunisolar.magma.basics.*; // NOSONAR
+import eu.lunisolar.magma.basics.builder.*; // NOSONAR
+import eu.lunisolar.magma.basics.exceptions.*; // NOSONAR
+import eu.lunisolar.magma.basics.meta.*; // NOSONAR
+import eu.lunisolar.magma.basics.meta.functional.*; // NOSONAR
+import eu.lunisolar.magma.basics.meta.functional.type.*; // NOSONAR
+import eu.lunisolar.magma.basics.meta.functional.domain.*; // NOSONAR
+import eu.lunisolar.magma.func.*; // NOSONAR
+import eu.lunisolar.magma.func.supp.memento.*; // NOSONAR
+import eu.lunisolar.magma.struct.tuple.*;
+
+import eu.lunisolar.magma.func.action.*; // NOSONAR
+import eu.lunisolar.magma.func.consumer.*; // NOSONAR
+import eu.lunisolar.magma.func.consumer.primitives.*; // NOSONAR
+import eu.lunisolar.magma.func.consumer.primitives.bi.*; // NOSONAR
+import eu.lunisolar.magma.func.consumer.primitives.obj.*; // NOSONAR
+import eu.lunisolar.magma.func.consumer.primitives.tri.*; // NOSONAR
+import eu.lunisolar.magma.func.function.*; // NOSONAR
+import eu.lunisolar.magma.func.function.conversion.*; // NOSONAR
+import eu.lunisolar.magma.func.function.from.*; // NOSONAR
+import eu.lunisolar.magma.func.function.to.*; // NOSONAR
+import eu.lunisolar.magma.func.operator.binary.*; // NOSONAR
+import eu.lunisolar.magma.func.operator.ternary.*; // NOSONAR
+import eu.lunisolar.magma.func.operator.unary.*; // NOSONAR
+import eu.lunisolar.magma.func.predicate.*; // NOSONAR
+import eu.lunisolar.magma.func.supplier.*; // NOSONAR
+
+/**
+ * Evaluates value only once, on first use.
+ */
+@SuppressWarnings("UnusedDeclaration")
+public class LazyCharX<X extends Throwable> extends LCharSupMementoX<X> implements LCharSingle {
+
+	protected LazyCharX(LCharSupplierX<X> function) {
+		super(function);
+	}
+
+	public static <X extends Throwable> LazyCharX<X> lazyValue(LCharSupplierX<X> supplier) {
+		return new LazyCharX<X>(supplier);
+	}
+
+	@Override
+	public char doGetAsChar() throws X {
+		if (function != null) {
+			lastValue = function.doGetAsChar();
+			function = null;
+		}
+
+		return lastValue;
+	}
+
+	public char value() {
+		return shovingDoGetAsChar();
+	}
+
+	public static boolean argEquals(LazyCharX the, Object that) {
+		return Null.equals(the, that, (one, two) -> {
+			if (!(one.getClass() != two.getClass())) {
+				return false;
+			}
+
+			LazyCharX other = (LazyCharX) two;
+
+			return one.value() == other.value();
+		});
+	}
+
+	public boolean equals(Object that) {
+		return argEquals(this, that);
+	}
+
+	@Override
+	public int hashCode() {
+		return LCharSingle.argHashCode(value());
+	}
+
+}
