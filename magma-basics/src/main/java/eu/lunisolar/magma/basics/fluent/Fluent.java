2c8deb0e61e1e0782b1575ec5850615af118236e
lunisolar <open@lunisolar.eu>
2019-08-04 00:47:39 +0200

    fluentInterjection

--- magma-basics/src/main/java/eu/lunisolar/magma/basics/fluent/Fluent.java
+++ magma-basics/src/main/java/eu/lunisolar/magma/basics/fluent/Fluent.java
@@ -20,5 +20,22 @@ package eu.lunisolar.magma.basics.fluent;
 
 import eu.lunisolar.magma.basics.meta.SelfReferencing;
 
+import java.util.function.*;
+
 public interface Fluent<SELF extends Fluent<SELF>> extends SelfReferencing<SELF> {
+
+    /** Non-capturing (if used properly) interjection in fluent calls. Please mind the boxing. */
+    default <T> SELF fluentInterjection(T ctx, Consumer<SELF> injection) {
+        final SELF self = self();
+        injection.accept(self);
+        return self;
+    }
+
+    /** Non-capturing (if used properly) interjection in fluent calls. Please mind the boxing. */
+    default <T> SELF fluentInterjection(T ctx, BiConsumer<SELF, T> injection) {
+        final SELF self = self();
+        injection.accept(self, ctx);
+        return self;
+    }
+
 }
