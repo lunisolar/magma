67af5e8ebd573416d7e6c74218bec4c2d308ed16
lunisolar <open@lunisolar.eu>
2019-10-02 21:58:03 +0200

    Opt

---
+++ magma-examples/src/test/java/eu/lunisolar/magma/examples/ValidationsTest.java
@@ -0,0 +1,31 @@
+/*
+ * This file is part of "lunisolar-magma".
+ *
+ * (C) Copyright 2014-2019 Lunisolar (http://lunisolar.eu/).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package eu.lunisolar.magma.examples;
+
+import eu.lunisolar.magma.func.supp.Be;
+import eu.lunisolar.magma.func.supp.Validations;
+
+public class ValidationsTest {
+
+    static {
+        Object str = null;
+        Validations.value(str, "someValue").must(Be::equal, "expectecStr", "Must be equal 'aaa'");
+    }
+
+}
